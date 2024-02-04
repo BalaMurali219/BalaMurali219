7759767e784a7ab1522c1c90d1e62624e75247c5
BalaMurali219 <158822404+BalaMurali219@users.noreply.github.com>
2024-02-04 20:47:03 +0530

    Create Username.java

---
+++ Username.java
@@ -0,0 +1,13 @@
+Import java.util.Scanner;
+Class Username
+{
+      Public static void main(String[] args)
+      {
+            String user;
+            System.out.print(“Enter the user name : “);
+            Scanner input = new Scanner(System.in);
+            User = input.nextLine();
+            input.close();
+            System.out.print(“Hello, “+user);
+      }
+} 
